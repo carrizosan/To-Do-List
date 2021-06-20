50680c9810ef59689a2e829e277692423ebc2903
carrizosan <santiicaarrizo@gmail.com>
2021-06-20 05:08:46 -0300

    To-do list version 1.0

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState } from 'react';
+import { StyleSheet, Text, SafeAreaView, View, Modal, Pressable } from 'react-native';
+import AddItem from './components/AddItem/AddItem';
+import ItemList from './components/ItemList/ItemList';
+import ConfirmModal from './components/ConfirmModal/ConfirmModal';
 
 export default function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [itemList, setItemList] = useState([]);
+  const [selectedItem, setSelectedItem] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleAddItem = () => {
+    if (inputValue.trim().length > 0) {
+      setItemList([
+        ...itemList,
+        {
+          id: Math.random().toString(),
+          value: inputValue,
+        },
+      ]);
+      setInputValue('');
+    }
+  };
+
+  const handleDeleteItem = () => {
+    setItemList(itemList.filter((item) => item.id !== selectedItem.id));
+    setSelectedItem({});
+    setModalVisible(false);
+  };
+
+  const handleConfirmDelete = (item) => {
+    setSelectedItem(item);
+    setModalVisible(true);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container, styles.androidSafeArea]}>
+      <ConfirmModal
+        title={selectedItem.value}
+        text='¿Esta seguro que desea eliminar el item?'
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        handleDeleteItem={handleDeleteItem}
+      />
+      <AddItem value={inputValue} onChangeText={setInputValue} handleAddItem={handleAddItem} />
+      <ItemList itemList={itemList} handleConfirmDelete={handleConfirmDelete} />
+      <StatusBar style='auto' />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#CEE5D0',
+  },
+  androidSafeArea: {
+    paddingTop: Platform.OS === 'android' ? 30 : 0,
   },
 });
